7214ef29333f96a2346709750bb7c8134f4834b1
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2019-05-15 13:32:55 +0200

    Avances spaceController

--- src/main/java/com/major/aplicacion/controller/SpaceController.java
+++ src/main/java/com/major/aplicacion/controller/SpaceController.java
@@ -1,6 +1,9 @@
 package com.major.aplicacion.controller;
 
+import com.major.aplicacion.Session.Cache;
+import com.major.aplicacion.Session.SessionInfo;
 import com.major.aplicacion.dtos.SpaceInfoDto;
+import com.major.aplicacion.dtos.UserDto;
 import com.major.aplicacion.messages.MessageBroker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,9 +22,17 @@ public class SpaceController {
 
     private static Logger logger = LoggerFactory.getLogger(SpaceController.class);
 
+    private Boolean isAdmin(String token) {
+        return token != null && Cache.getItem(token).isPresent() && ((SessionInfo) Cache.getItem(token).get()).getRol() == UserDto.Rol.ADMIN;
+    }
+
     @RequestMapping(value = "/spaces", method = RequestMethod.GET)
     public ResponseEntity getAllSpaces(@CookieValue(value = "token", required = false) String token) throws IOException {
         logger.info("GET\t/spaces request received");
+
+        if (token == null || !Cache.containsToken(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         // TODO
 
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
@@ -30,6 +41,10 @@ public class SpaceController {
     @RequestMapping(value = "/space/{id}", method = RequestMethod.GET)
     public ResponseEntity getSpaceById(@CookieValue(value = "token", required = false) String token, @PathVariable long id) throws IOException {
         logger.info("GET\t/space/{id} request received");
+
+        if (token == null || !Cache.containsToken(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         // TODO
 
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
@@ -38,6 +53,11 @@ public class SpaceController {
     @RequestMapping(value = "/space/{id}", method = RequestMethod.PUT)
     public ResponseEntity putSpaceById(@CookieValue(value = "token", required = false) String token, @PathVariable long id, SpaceInfoDto spaceInfoDto) throws IOException {
         logger.info("GET\t/space/{id} request received");
+
+        if (token == null || !Cache.containsToken(token) || !isAdmin(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
         // TODO
 
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
@@ -46,6 +66,11 @@ public class SpaceController {
     @RequestMapping(value = "/space/{id}/bookings", method = RequestMethod.GET)
     public ResponseEntity getSpaceBookingsById(@CookieValue(value = "token", required = false) String token, @PathVariable long id) throws IOException {
         logger.info("GET\t/space/{id}/bookings request received");
+
+        if (token == null || !Cache.containsToken(token)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
         // TODO
 
         return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
